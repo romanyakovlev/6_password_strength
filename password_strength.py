3bbd9c11b648c53799f59302c92151c67062b530
Roman <yakovlevroman8@gmail.com>
2016-10-16 17:59:14 +0700

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -3,7 +3,7 @@ import string
 
 def get_password_strength(password):
 
-    with open('password_blacklist.txt', 'r') as file_pass:
+    with open('password_strength.txt', 'r') as file_pass:
         pass_black_list = file_pass.read()
     file_pass.closed
     pass_black_list = pass_black_list.split('\n')
@@ -17,13 +17,15 @@ def get_password_strength(password):
         string.digits,
         string.punctuation,
         string.punctuation+string.digits,
-        
+
 ]
 
-    string_pass_list = [password]*4 + [password[1:-1]]
+    string_pass_list = [password]*4+[password[1:-1]]
 
-    pass_complexity += sum(any(symbol for symbol  in y if symbol  in x ) for x, y in zip(string_pass_list,string_pass_params))
+    complexity_counter = any(symbol for symbol  in pass_string if symbol  in check_values )
+    for check_values, pass_string in zip(string_pass_list,string_pass_params)
 
+    pass_complexity += sum(complexity_counter)
     pass_complexity += password not in pass_black_list
 
     if len(password) < 8 and len(password) > 4:
@@ -37,7 +39,9 @@ def get_password_strength(password):
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('password_text')
     password_text = parser.parse_args().password_text
+
     print(get_password_strength(password_text))
